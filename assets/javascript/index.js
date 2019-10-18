d5a8b43e0bcda6ac9d38ed472cb249adce4509f7
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-10-18 17:15:26 +0200

    add basic question loading and rendering

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,19 +1,59 @@
-function quiz(questions) {
-  let quiz = {};
+var quiz = function(questions) {
+  let index = 0;
+  return {
+    questions: questions,
+    app: document.querySelector("#app"),
+    init: function() {
+      try {
+        this.loadQ();
+      } catch (error) {
+        this.end();
+      }
+    },
+    loadQ: function() {
+      let p = document.createElement("p");
+      // this div will be the cardbody
+      let divCardBody = document.createElement("div");
+      divCardBody.classList.add("card-body");
+      // and this div will be the container for the cardbody
+      let divCard = document.createElement("div");
+      divCard.classList.add("card");
+      p.textContent = this.questions[index].title;
+      p.setAttribute("data-question-index", index);
+      divCardBody.append(p);
+      divCard.append(divCardBody);
+      this.questions[index].choices.forEach((element, i) => {
+        let btn = document.createElement("button");
+        btn.textContent = element;
+        btn.setAttribute("data-index", i);
+        btn.classList.add("btn", "btn-warning");
+        divCardBody.append(btn);
+      });
+      this.app.append(divCard);
+      index++;
+    },
+    end: function() {
+      let divCard = document.createElement("div");
+      divCard.classList.add("card");
 
-  quiz.questions = questions;
+      let divCardBody = document.createElement("div");
+      divCardBody.classList.add("card-body");
 
-  return quiz;
-}
+      let form = document.createElement("form");
+      let input = document.createElement("input");
+      input.textContent = "Your name";
+      let button = document.createElement("button");
+      button.textContent = "Submit";
+      button.classList.add("btn", "btn-warning");
+      form.append(input, button);
+      divCardBody.append(form);
+      divCard.append(divCardBody);
+      this.app.append(divCard);
+    }
+  };
+};
 
-let quiz0 = quiz(questions);
+let game = quiz(questions);
 document.querySelector("#start").addEventListener("click", function() {
-  let p = document.createElement("p");
-  p.textContent = quiz0.questions[0].title;
-  quiz0.questions[0].choices.forEach(element => {
-    let btn = document.createElement("button");
-    btn.textContent = element;
-    p.append(btn);
-  });
-  document.querySelector("#app").append(p);
+  game.init();
 });
